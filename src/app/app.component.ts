cb0b48620ee63a7a5c8cb24d217cbfa7d2d0defd
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2023-04-19 17:13:09 -0500

    feat: implement mouse follower

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Mouse Follower</h1>`,
+  template: `
+    <h1>Mouse Follower</h1>
+    <button (click)="onToggleButton()">
+      {{ isMouseFollowerEnabled ? 'Disable' : 'Enable' }} mouse follower
+    </button>
+    <app-mouse-follower [isEnabled]="isMouseFollowerEnabled" />
+  `,
 })
-export class AppComponent {}
+export class AppComponent {
+  isMouseFollowerEnabled: boolean = false;
+  onToggleButton() {
+    this.isMouseFollowerEnabled = !this.isMouseFollowerEnabled;
+  }
+}
--- src/app/shared/components/mouse-follower/mouse-follower.component.ts
+++ src/app/shared/components/mouse-follower/mouse-follower.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  NgZone,
+  OnChanges,
+  OnDestroy,
+  Renderer2,
+  SimpleChange,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,8 +17,41 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './mouse-follower.component.html',
-  styleUrls: ['./mouse-follower.component.css']
+  styleUrls: ['./mouse-follower.component.css'],
 })
-export class MouseFollowerComponent {
+export class MouseFollowerComponent implements OnChanges, OnDestroy {
+  @Input() isEnabled: boolean = false;
 
+  @ViewChild('mouseFollower')
+  mouseFollowerEl: ElementRef | null = null;
+  private unlistenMouseMove: () => void = () => null;
+
+  constructor(private ngZone: NgZone, private renderer2: Renderer2) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const isEnabled: SimpleChange = changes['isEnabled'];
+    const mustUnlistenMouseMove =
+      isEnabled.previousValue === true &&
+      !isEnabled.currentValue &&
+      this.unlistenMouseMove() === undefined;
+
+    mustUnlistenMouseMove && this.unlistenMouseMove();
+
+    if (!isEnabled.currentValue) return;
+
+    this.ngZone.runOutsideAngular(() => {
+      this.unlistenMouseMove = this.renderer2.listen(
+        'document',
+        'mousemove',
+        (e: MouseEvent) => {
+          if (!this.mouseFollowerEl) return;
+          this.mouseFollowerEl.nativeElement.style.translate = `${e.x}px ${e.y}px`;
+        }
+      );
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unlistenMouseMove();
+  }
 }
